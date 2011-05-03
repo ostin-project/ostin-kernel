960c9ec7df227f08fc167931afa4f5740cd2e4fe
Mike Gelfand <mikedld@mikedld.com>
2011-05-04 00:45:10 +0300

    consistent copyright headers with correct years (I hope) still not that good when it comes to Menuet copyrights extracted authors into separate file

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,59 +1,19 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;======================================================================================================================
+;;///// kernel.asm ///////////////////////////////////////////////////////////////////////////////////////// GPLv2 /////
+;;======================================================================================================================
+;; (c) 2004-2010 KolibriOS team <http://kolibrios.org/>
+;; (c) 2000-2004 MenuetOS <http://menuetos.net/>
+;;======================================================================================================================
+;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+;; License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later
+;; version.
 ;;
-;; Copyright (C) KolibriOS team 2004-2010. All rights reserved.
-;; PROGRAMMING:
-;; Ivan Poddubny
-;; Marat Zakiyanov (Mario79)
-;; VaStaNi
-;; Trans
-;; Mihail Semenyako (mike.dld)
-;; Sergey Kuzmin (Wildwest)
-;; Andrey Halyavin (halyavin)
-;; Mihail Lisovin (Mihasik)
-;; Andrey Ignatiev (andrew_programmer)
-;; NoName
-;; Evgeny Grechnikov (Diamond)
-;; Iliya Mihailov (Ghost)
-;; Sergey Semyonov (Serge)
-;; Johnny_B
-;; SPraid (simba)
-;; Hidnplayr
-;; Alexey Teplov (<Lrz>)
+;; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 ;;
-;; Data in this file was originally part of MenuetOS project which is
-;; distributed under the terms of GNU GPL. It is modified and redistributed as
-;; part of KolibriOS project under the terms of GNU GPL.
-;;
-;; Copyright (C) MenuetOS 2000-2004 Ville Mikael Turjanmaa
-;; PROGRAMMING:
-;;
-;; Ville Mikael Turjanmaa, villemt@itu.jyu.fi
-;; - main os coding/design
-;; Jan-Michael Brummer, BUZZ2@gmx.de
-;; Felix Kaiser, info@felix-kaiser.de
-;; Paolo Minazzi, paolo.minazzi@inwind.it
-;; quickcode@mail.ru
-;; Alexey, kgaz@crosswinds.net
-;; Juan M. Caravaca, bitrider@wanadoo.es
-;; kristol@nic.fi
-;; Mike Hibbett, mikeh@oceanfree.net
-;; Lasse Kuusijarvi, kuusijar@lut.fi
-;; Jarek Pelczar, jarekp3@wp.pl
-;;
-;; KolibriOS is distributed in the hope that it will be useful, but WITHOUT ANY
-;; WARRANTY. No author or distributor accepts responsibility to anyone for the
-;; consequences of using it or for whether it serves any particular purpose or
-;; works at all, unless he says so in writing. Refer to the GNU General Public
-;; License (the "GPL") for full details.
-;
-;; Everyone is granted permission to copy, modify and redistribute KolibriOS,
-;; but only under the conditions described in the GPL. A copy of this license
-;; is supposed to have been given to you along with KolibriOS so you can know
-;; your rights and responsibilities. It should be in a file named COPYING.
-;; Among other things, the copyright notice and this notice must be preserved
-;; on all copies.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; You should have received a copy of the GNU General Public License along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;======================================================================================================================
 
 include 'macros.inc'
 
