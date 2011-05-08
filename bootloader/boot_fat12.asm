425804fda65d8dca557184b44bdd41add1ce16b5
Mike Gelfand <mikedld@mikedld.com>
2011-05-09 00:24:49 +0300

    more copyrights on bootloader/drivers/skin

--- bootloader/boot_fat12.asm
+++ bootloader/boot_fat12.asm
@@ -1,12 +1,19 @@
-; FAT12 boot sector for Kolibri OS
-;
-; Copyright (C) Alex Nogueira Teixeira
-; Copyright (C) Diamond
-; Copyright (C) Dmitry Kartashov aka shurf
-;
-; Distributed under GPL, see file COPYING for details
-;
-; Version 1.0
+;;======================================================================================================================
+;;///// boot_fat32.asm ///////////////////////////////////////////////////////////////////////////////////// GPLv2 /////
+;;======================================================================================================================
+;; (c) 2007-2011 KolibriOS team <http://kolibrios.org/>
+;; (c) 2002 Alex Nogueira Teixeira <alexwal@siteplanet.com.br>
+;;======================================================================================================================
+;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+;; License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;======================================================================================================================
 
 lf		equ	0ah
 cr		equ	0dh
--- drivers/mixer.asm
+++ drivers/mixer.asm
@@ -1,13 +1,19 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                              ;;
-;; Copyright (C) KolibriOS team 2006-2008. All rights reserved. ;;
-;; Distributed under terms of the GNU General Public License    ;;
-;;                                                              ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;   (C) copyright Serge 2006
-;   email: infinity_sound@mail.ru
-
+;;======================================================================================================================
+;;///// mixer.asm ////////////////////////////////////////////////////////////////////////////////////////// GPLv2 /////
+;;======================================================================================================================
+;; (c) 2006-2010 KolibriOS team <http://kolibrios.org/>
+;; (c) 2006 Serge <infinity_sound@mail.ru>
+;;======================================================================================================================
+;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+;; License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;======================================================================================================================
 
 align 4
 
